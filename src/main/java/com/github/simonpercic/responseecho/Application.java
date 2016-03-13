353788b797e233e0643bce4d1941436a7fb385b9
Simon Perčič <simon@percic.si>
2016-03-13 20:33:14 +0100

    Pretty printing response string

--- src/main/java/com/github/simonpercic/responseecho/Application.java
+++ src/main/java/com/github/simonpercic/responseecho/Application.java
@@ -1,13 +1,22 @@
 package com.github.simonpercic.responseecho;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author Simon Percic <a href="https://github.com/simonpercic">https://github.com/simonpercic</a>
  */
 @SpringBootApplication
 public class Application {
+
+    @Bean
+    public Gson gson() {
+        return new GsonBuilder().setPrettyPrinting().create();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/com/github/simonpercic/responseecho/MainController.java
+++ src/main/java/com/github/simonpercic/responseecho/MainController.java
@@ -1,5 +1,12 @@
 package com.github.simonpercic.responseecho;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +25,15 @@ import java.util.zip.GZIPInputStream;
  */
 @RestController
 public class MainController {
+
+    private final Gson gson;
+    private final JsonParser jsonParser;
+
+    @Autowired
+    public MainController(Gson gson) {
+        this.gson = gson;
+        this.jsonParser = new JsonParser();
+    }
 
     @RequestMapping(value = "/re/{response}", method = RequestMethod.GET, produces = "application/json")
     public @ResponseBody String echoResponse(@PathVariable(value = "response") String response) throws IOException {
@@ -42,6 +58,26 @@ public class MainController {
         gzipInputStream.close();
         bis.close();
 
-        return sb.toString();
+        String json = sb.toString();
+        return toPrettyFormat(json);
+    }
+
+    /**
+     * Try to pretty-print the response string.
+     *
+     * @param responseString response string
+     * @return pretty-printed response string
+     */
+    private String toPrettyFormat(String responseString) {
+        if (StringUtils.isEmpty(responseString)) {
+            return "";
+        }
+
+        try {
+            JsonElement jsonElement = jsonParser.parse(responseString);
+            return gson.toJson(jsonElement);
+        } catch (JsonSyntaxException e) {
+            return responseString;
+        }
     }
 }
